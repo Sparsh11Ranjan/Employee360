a4bd779a7b03cd4ef7b7f5f3505b9caf10951645
Sparsh11Ranjan <145247163+Sparsh11Ranjan@users.noreply.github.com>
2025-07-09 01:56:38 +0530

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL,
+{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectToDatabase;
